bccafc221d56643a07ebe34e02895eed7b874284
Florian <flostylefr@gmail.com>
2026-08-27 15:02:15 +0200

    fix: remove HTML comments from JS files (syntax errors)

--- src/utils.js
+++ src/utils.js
@@ -54,7 +54,7 @@ class Utils {
 
   // TODO: Handle releasese from Ko-fi
   static getLatestPlugin() {
-    return 'https://github.com/FloStyle/m7-jellyfin/releases/latest/download/jellyfin.zip'; <!-- RENAME-TRACKER: Update to m7-jellyfin-neo -->
+    return 'https://github.com/FloStyle/m7-jellyfin/releases/latest/download/jellyfin.zip';
   }
 
   ticksToDate(input = 0) {
